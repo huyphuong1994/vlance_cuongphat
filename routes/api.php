81a00fd40d559d3eb2a6dd2316e8219a75506b44
daophuong <phuongdh@caro.vn>
2023-06-21 21:14:16 +0700

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('set-slug-auto-product', [ProductController::class, 'setSlugAutoProduct']);
-Route::post('set-slug-auto-category', [CategoryController::class, 'setSlugAutoCategory']);
+//Route::post('set-slug-auto-product', [ProductController::class, 'setSlugAutoProduct']);
+//Route::post('set-slug-auto-category', [CategoryController::class, 'setSlugAutoCategory']);
 
 Route::prefix('v100')->group(function () {
     Route::middleware(['CheckApiKey'])->group(function () {
